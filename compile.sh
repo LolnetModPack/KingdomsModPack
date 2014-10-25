7f235646d792c1db2e158ce8a38879cfda284435
James137137 <james137137@gmail.com>
2014-10-25 14:55:01 +1300

    Complier should be 100% Auto now

--- compile.sh
+++ compile.sh
@@ -1,6 +1,14 @@
-#java -cp SKCraftLauncher.jar com.skcraft.launcher.builder.PackageBuilder --version "1.0" --manifest-dest "dist/manifest.json" --objects-dest "dist/objects" --files "src" --config "modpack.json" --objects-url "http://modpacks-lolnet.rhcloud.com/modpack/objects" --version-file "version.json" --libs-url "http://modpacks-lolnet.rhcloud.com/modpack/libraries"
+array=( $(< BuildNumber) )
+sum=$( IFS=+; echo "${array[*]}" | bc )
+echo "$(($sum+1))" > BuildNumber
+
+java -cp LolnetLauncher.jar com.skcraft.launcher.builder.PackageBuilder --version "1.2.2" --manifest-dest "dist/manifest.json" --objects-dest "dist/objects" --files "src" --config "modpack.json" --objects-url "https://lolnet.co.nz/modpack/objects" --version-file "version.json" --libs-url "https://lolnet.co.nz/modpack/libraries"
+
 
 java -cp SKCraftLauncher.jar com.skcraft.launcher.builder.PackageBuilder --version "1.0" --manifest-dest "dist/manifest.json" --objects-dest "dist/objects" --files "src" --config "modpack.json" --objects-url "https://www.lolnet.co.nz/modpack/objects" --version-file "version.json" --libs-url "https://www.lolnet.co.nz/modpack/libraries"
 cp -r /var/www/modpack/Setup/Kingdoms/dist/objects/ /var/www/modpack/
 cp -r /var/www/modpack/Setup/Kingdoms/dist/manifest.json /var/www/modpack/kingdomsmanifest.json
 rm -r /var/www/modpack/Setup/Kingdoms/dist/
+
+cp CompletedTemplate.json ../../public/Kingdoms.json 
+rm CompletedTemplate.json
